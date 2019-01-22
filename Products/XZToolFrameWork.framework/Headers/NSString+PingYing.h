98c1f6a88496bfb83d81f34b892228ee79e274e0
Mephisto <xiezefu@outlook.com>
2019-01-22 11:35:51 +0800

    初始化代码仓库

---
+++ Products/XZToolFrameWork.framework/Headers/NSString+PingYing.h
@@ -0,0 +1,18 @@
+//
+//  NSString+PingYing.h
+//  Transportation
+//
+//  Created by mac on 2018/4/2.
+//  Copyright © 2018年 Mephsito. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (PingYing)
+
++ (NSString *)getFirstLetterFromString:(NSString *)aString;
+
+- (NSString *)getPingYing;
+
+
+@end
